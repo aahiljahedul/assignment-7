00cd796b6fa14732a1ab5b79fff61e346a64e6e0
jahan-meg <delwarhossainbokaul@gmail.com>
2025-05-07 21:27:22 +0600

    add this update file

---
+++ auction-gallery/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>hello</div>
+      <div><Navbar></Navbar></div>
+      <div><Hero></Hero></div>
+      <div><Bids></Bids></div>
+      <div><Footer></Footer></div>
+      <div></div>
+    </>
+  )
+}
+
+export default App
